7d35760c6e2736021b0289427dcc1f0be5e84815
Ana María Bedoya <anabedoya1997@gmail.com>
2020-11-27 11:41:45 -0500

    Configuring images routes properly

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
     def create
         property = current_user.properties.find(params[:property_id])
         property.images.create
-        redirect_to properties_path
+        redirect_to property_path(params[:property_id])
     end
 
     def update
@@ -10,7 +10,7 @@ class ImagesController < ApplicationController
         image = property.images.find(params[:id])
 
         if image.image.attach(image_params[:image])
-            redirect_to properties_path
+            redirect_to property_path(params[:property_id])
         else 
             render :edit
         end
